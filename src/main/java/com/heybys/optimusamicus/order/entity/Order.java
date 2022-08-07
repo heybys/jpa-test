5d5f94db1fa03bccc73eb26567a9011757140ee6
heybys <byn0174@gmail.com>
2022-08-08 02:45:19 +0900

    update entity

--- src/main/java/com/heybys/optimusamicus/order/entity/Order.java
+++ src/main/java/com/heybys/optimusamicus/order/entity/Order.java
@@ -1,5 +1,6 @@
 package com.heybys.optimusamicus.order.entity;
 
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,6 +11,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.Hibernate;
 
 @Getter
 @Entity
@@ -18,7 +20,7 @@ import lombok.NoArgsConstructor;
 public class Order {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "orderId", nullable = false)
   private Long orderId;
 
@@ -29,5 +31,22 @@ public class Order {
   public Order(Long orderId, String serialNumber) {
     this.orderId = orderId;
     this.serialNumber = serialNumber;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
+      return false;
+    }
+    Order order = (Order) o;
+    return orderId != null && Objects.equals(orderId, order.orderId);
+  }
+
+  @Override
+  public int hashCode() {
+    return getClass().hashCode();
   }
 }
--- src/main/java/com/heybys/optimusamicus/user/entity/User.java
+++ src/main/java/com/heybys/optimusamicus/user/entity/User.java
@@ -1,5 +1,6 @@
 package com.heybys.optimusamicus.user.entity;
 
+import java.math.BigDecimal;
 import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -31,6 +32,10 @@ public class User {
   @Setter
   @Column(name = "username", nullable = false, unique = true)
   private String username;
+
+  @Setter
+  @Column(name = "ratio", precision = 28, scale = 5)
+  private BigDecimal ratio;
 
   @Setter
   @Enumerated(EnumType.STRING)
