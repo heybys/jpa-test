e3856ab0c7a23b3f393219d7244a0c8c27d9b991
Jero <heybys1@lgcns.com>
2022-08-29 14:22:46 +0900

    add type to uuid column

--- src/main/java/com/heybys/optimusamicus/order/entity/Order.java
+++ src/main/java/com/heybys/optimusamicus/order/entity/Order.java
@@ -16,6 +16,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.Hibernate;
+import org.hibernate.annotations.Type;
 
 @Getter
 @Setter
@@ -35,6 +36,7 @@ public class Order extends BaseEntity {
   @Column(name = "id")
   private Long id;
 
+  @Type(type = "org.hibernate.type.UUIDCharType")
   @Column(name = "serial_number", nullable = false)
   private UUID serialNumber = UUID.randomUUID();
 
