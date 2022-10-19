1bdbcff49b59493c49c890a75e25c90e5e94cd5b
heybys <byn0174@gmail.com>
2022-10-20 02:28:07 +0900

    test code

--- src/main/java/com/heybys/optimusamicus/order/repository/CustomOrderRepositoryImpl.java
+++ src/main/java/com/heybys/optimusamicus/order/repository/CustomOrderRepositoryImpl.java
@@ -5,10 +5,15 @@ import static com.heybys.optimusamicus.order.entity.QOrder.order;
 import com.heybys.optimusamicus.order.entity.Order;
 import com.heybys.optimusamicus.order.feign.FeignTest;
 import com.heybys.optimusamicus.order.repository.support.OrderQuerydslRepositorySupport;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.jdbc.support.JdbcAccessor;
 import org.springframework.stereotype.Repository;
 
 @Slf4j
@@ -18,6 +23,10 @@ public class CustomOrderRepositoryImpl extends OrderQuerydslRepositorySupport
     implements CustomOrderRepository {
 
   private final JdbcTemplate jdbcTemplate;
+
+  private final JdbcAccessor jdbcAccessor;
+
+  private final DataSource dataSource;
 
   private final FeignTest feignTest;
 
@@ -34,6 +43,16 @@ public class CustomOrderRepositoryImpl extends OrderQuerydslRepositorySupport
 
   // @Modifying(flushAutomatically = true, clearAutomatically = true)
   private void insertQuery(Order orderEntity) {
+    try {
+      Connection c = DataSourceUtils.getConnection(dataSource);
+      boolean autoCommit = c.getAutoCommit();
+      log.debug("autoCommit : {}", autoCommit);
+
+      c.setAutoCommit(true);
+
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
 
     int affectedRows =
         jdbcTemplate.update(
@@ -42,32 +61,32 @@ public class CustomOrderRepositoryImpl extends OrderQuerydslRepositorySupport
             orderEntity.getName(),
             orderEntity.getSerialNumber().toString());
 
-    // EntityManager entityManager = getEntityManager();
-    // assert entityManager != null;
-    // int affectedRows =
-    //     entityManager
-    //         .createNativeQuery(
-    //             "       INSERT INTO orders "
-    //                 + "  (order_id, name, serial_number)"
-    //                 + " VALUES "
-    //                 + "  (:id, :name, :serialNumber)")
-    //         .setParameter("id", orderEntity.getId())
-    //         .setParameter("name", orderEntity.getName())
-    //         .setParameter("serialNumber", orderEntity.getSerialNumber().toString())
-    //         .executeUpdate();
-    // long affectedRows =
-    //     queryFactory.insert(order).columns(order.id, order.name).values(1L, "test").execute();
-    // .set(order.id, orderEntity.getId())
-    // .set(order.serialNumber, orderEntity.getSerialNumber())
-    // .set(order.name, orderEntity.getName())
-    // queryFactory
-    //     .insert(order)
-    //     .columns(order.id, order.serialNumber, order.name)
-    //     .values(orderEntity.getId(), orderEntity.getSerialNumber(), orderEntity.getName())
-    //     .execute();
-
     if (affectedRows != 1) {
       throw new IllegalArgumentException();
     }
   }
 }
+
+// EntityManager entityManager = getEntityManager();
+// assert entityManager != null;
+// int affectedRows =
+//     entityManager
+//         .createNativeQuery(
+//             "       INSERT INTO orders "
+//                 + "  (order_id, name, serial_number)"
+//                 + " VALUES "
+//                 + "  (:id, :name, :serialNumber)")
+//         .setParameter("id", orderEntity.getId())
+//         .setParameter("name", orderEntity.getName())
+//         .setParameter("serialNumber", orderEntity.getSerialNumber().toString())
+//         .executeUpdate();
+// long affectedRows =
+//     queryFactory.insert(order).columns(order.id, order.name).values(1L, "test").execute();
+// .set(order.id, orderEntity.getId())
+// .set(order.serialNumber, orderEntity.getSerialNumber())
+// .set(order.name, orderEntity.getName())
+// queryFactory
+//     .insert(order)
+//     .columns(order.id, order.serialNumber, order.name)
+//     .values(orderEntity.getId(), orderEntity.getSerialNumber(), orderEntity.getName())
+//     .execute();
