4c6ebf00602f1a9fd894cc8fd046dd7262bd0a36
heybys <byn0174@gmail.com>
2022-10-27 00:44:06 +0900

    add cors config

--- src/main/java/com/heybys/optimusamicus/common/config/WebConfig.java
+++ src/main/java/com/heybys/optimusamicus/common/config/WebConfig.java
@@ -5,11 +5,22 @@ import javax.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry
+        .addMapping("/**")
+        .allowedMethods("*")
+        .allowedOrigins("http://localhost:3000")
+        .allowedHeaders("*")
+        .allowCredentials(true);
+  }
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {}
