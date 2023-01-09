b5f309a6e174d95ac3b25e0956b7ee1f853f4fed
heybys <byn0174@gmail.com>
2023-01-09 18:51:00 +0900

    apply fetch join for Menu and OptionGroups

--- src/main/java/com/heybys/optimusamicus/shop/domain/MenuRepository.java
+++ src/main/java/com/heybys/optimusamicus/shop/domain/MenuRepository.java
@@ -1,12 +1,14 @@
 package com.heybys.optimusamicus.shop.domain;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
 
+  @EntityGraph(attributePaths = {"optionGroups"})
   List<Menu> findByShopId(Long shopId);
 
 }
