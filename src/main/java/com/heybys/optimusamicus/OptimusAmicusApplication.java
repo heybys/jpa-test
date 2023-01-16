4c0a00c2184cc6bf7674e523cfc2f09ac59fd0a9
heybys <byn0174@gmail.com>
2023-01-17 02:05:31 +0900

    add spring-security but disable securityAutoConfiguration

--- src/main/java/com/heybys/optimusamicus/OptimusAmicusApplication.java
+++ src/main/java/com/heybys/optimusamicus/OptimusAmicusApplication.java
@@ -2,13 +2,14 @@ package com.heybys.optimusamicus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 // @ServletComponentScan
 @EnableAsync
-@SpringBootApplication
 @EnableFeignClients
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class OptimusAmicusApplication {
 
   public static void main(String[] args) {
