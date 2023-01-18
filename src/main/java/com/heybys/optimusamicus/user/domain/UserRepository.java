b48b003ec045ce37f6744b951b1d5d20fa199e72
heybys <byn0174@gmail.com>
2023-01-19 03:28:22 +0900

    add UserDetailsService interface to UserService

--- src/main/java/com/heybys/optimusamicus/user/domain/UserRepository.java
+++ src/main/java/com/heybys/optimusamicus/user/domain/UserRepository.java
@@ -7,5 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+  Optional<User> findByUsername(String username);
+
   Optional<User> findByUsernameAndPassword(String username, String password);
 }
--- src/main/java/com/heybys/optimusamicus/user/service/UserService.java
+++ src/main/java/com/heybys/optimusamicus/user/service/UserService.java
@@ -5,13 +5,23 @@ import com.heybys.optimusamicus.user.domain.UserRepository;
 import com.heybys.optimusamicus.user.service.model.Credentials;
 import com.heybys.optimusamicus.user.service.model.RegisterUserInfo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
   private final UserRepository userRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = userRepository.findByUsername(username).orElseThrow();
+
+    return null;
+  }
 
   public void register(Credentials credentials, RegisterUserInfo registerUserInfo) {
     User user =
