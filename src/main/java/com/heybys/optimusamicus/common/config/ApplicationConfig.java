7e03149f0b19a5390692b16db0abecc7cd7fb9c8
heybys <byn0174@gmail.com>
2023-01-26 04:52:19 +0900

    remove AuthService

--- src/main/java/com/heybys/optimusamicus/common/config/ApplicationConfig.java
+++ src/main/java/com/heybys/optimusamicus/common/config/ApplicationConfig.java
@@ -1,31 +1,10 @@
 package com.heybys.optimusamicus.common.config;
 
-import com.heybys.optimusamicus.common.filter.AuthFactory;
-import com.heybys.optimusamicus.member.service.AuthService;
-import javax.servlet.Filter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
 
-  private final AuthFactory authFactory;
-
-  @Bean
-  public AuthService authService() {
-    return authFactory.getService();
-  }
-
-  // @Bean
-  public FilterRegistrationBean<Filter> authFilter() {
-    Filter authFilter = authFactory.getFilter();
-
-    FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
-    filterFilterRegistrationBean.setFilter(authFilter);
-    filterFilterRegistrationBean.addUrlPatterns("/*");
-    return filterFilterRegistrationBean;
-  }
 }
--- src/main/java/com/heybys/optimusamicus/member/service/AuthService.java
+++ src/main/java/com/heybys/optimusamicus/member/service/AuthService.java
@@ -1,11 +1,46 @@
 package com.heybys.optimusamicus.member.service;
 
+import com.heybys.optimusamicus.common.utils.HttpServletRequestProvider;
+import com.heybys.optimusamicus.common.utils.HttpServletResponseProvider;
+import com.heybys.optimusamicus.member.domain.Member;
+import com.heybys.optimusamicus.member.domain.MemberRepository;
 import com.heybys.optimusamicus.member.service.model.Credentials;
 import com.heybys.optimusamicus.member.service.model.SessionUserInfo;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-public interface AuthService {
+@Service
+@RequiredArgsConstructor
+public class AuthService {
 
-  SessionUserInfo login(Credentials credentials);
+  private final MemberRepository memberRepository;
 
-  void logout();
+  public SessionUserInfo login(Credentials credentials) {
+    Member member =
+        memberRepository
+            .findByUsernameAndPassword(credentials.getUsername(), credentials.getPassword())
+            .orElseThrow(
+                () ->
+                    new IllegalArgumentException(
+                        "There is no user with the username and password."));
+    SessionUserInfo sessionUserInfo = SessionUserInfo.from(member);
+    sessionUserInfo.addTo(HttpServletRequestProvider.getSession());
+
+    return sessionUserInfo;
+  }
+
+  public void logout() {
+    // invalidate session
+    HttpServletRequestProvider.getSession(false).invalidate();
+
+    // expire sessionId cookie
+    HttpServletResponse response = HttpServletResponseProvider.getResponse();
+    Cookie cookie = new Cookie("JSESSIONID", null);
+    cookie.setPath("/");
+    cookie.setHttpOnly(true);
+    cookie.setMaxAge(0);
+    response.addCookie(cookie);
+  }
 }
